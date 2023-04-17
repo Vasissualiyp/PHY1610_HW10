4a0fd5aa675db955bd3efb2924553b1790a31b53
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-04-17 11:03:01 -0400

    The assignment is complete. Post-processing python code has been modularized

--- winners.py
+++ winners.py
@@ -1,12 +1,39 @@
 import numpy as np
 import os
 
+# This function would obtain the winner in the given folder
+def getwinner(result):
+	with open(result, 'r') as f:
+		lines = f.read().splitlines()
+		last_line = lines[-1]
+		if last_line.startswith('winners='):
+			outcome = last_line.split('=')[1].strip()
+			if outcome == 'humans':
+				with open('humanswin.dat', 'a') as humans_file:
+					humans_file.write(i_str + ' ' + j_str + '\n')
+			elif outcome == 'zombies':
+				with open('zombieswin.dat', 'a') as zombies_file:
+					zombies_file.write(i_str + ' ' + j_str + '\n')
+
+# This function resets the humanswin.dat and zombieswin.dat
+def reset_winnerfiles():
+	if os.path.exists('humanswin.dat'):
+		os.remove('humanswin.dat')
+	if os.path.exists('zombieswin.dat'):
+		os.remove('zombieswin.dat')
+	with open('humanswin.dat', 'a') as humans_file:
+		humans_file.write('Y(0) Z(0)' + '\n')
+	with open('zombieswin.dat', 'a') as zombies_file:
+		zombies_file.write('Y(0) Z(0)' + '\n')
+
 # Get the output directory
 current_directory = os.getcwd()
 output_directory = os.path.join(current_directory, "output")
 
 # Create array with all the initial conditions
 ics_arr = np.linspace(0,1,51)
+
+reset_winnerfiles()
 
 # Main loop over all the parameters
 for i in ics_arr:
@@ -22,17 +49,6 @@ for i in ics_arr:
 		result = os.path.join(folder_directory, 'output.txt') 
 		
 		# Get the winner
-		with open(result, 'r') as f:
-			lines = f.read().splitlines()
-			last_line = lines[-1]
-			if last_line.startswith('winners='):
-			   outcome = last_line.split('=')[1].strip()
-			   if outcome == 'humans':
-			       result = 1
-			   elif outcome == 'zombies':
-			       result = 0
-			   else:
-			       result = None
-			   print(result)
+		getwinner(result)
 		
 
